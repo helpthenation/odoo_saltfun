e31e109b79fd67b658ddc6f4fa48300af138cbfa
imtoohard <info@odoochain.com>
2021-01-07 09:59:49 +0800

    Signed-off-by: imtoohard <info@odoochain.com>

---
+++ addons13/hr_leave_request_aliasing/static/src/js/web_planner_hr_leave.js
@@ -0,0 +1,10 @@
+// odoo.define('planner_hr_leave.planner', function (require) {
+
+/*
+odoo.define('hr_leave_request_aliasing.web_planner_hr_leave', function (require) {
+"use strict";
+
+var planner = require('web.planner.common');
+
+});
+*/
