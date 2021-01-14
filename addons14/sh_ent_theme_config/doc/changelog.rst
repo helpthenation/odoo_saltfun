c8b5afb783a67799f0708e1557588ec861557d4f
imtoohard <info@odoochain.com>
2021-01-14 09:14:02 +0800

    first commit github_connector

---
+++ addons14/sh_ent_theme_config/doc/changelog.rst
@@ -0,0 +1,17 @@
+14.0.1 (10 October 2020) 
+-------
+
+- initial release
+
+
+14.0.2 (10 November 2020) 
+-------
+
+- PWA Move Inside Module
+- Responsive Bug Fix
+
+
+14.0.2 (11 November 2020) 
+-------
+
+- RTL Issue Fix
--- addons14/website_date_of_birth/models/res_partner.py
+++ addons14/website_date_of_birth/models/res_partner.py
@@ -5,4 +5,4 @@ from odoo import api, fields, models, _
 class ResPartner(models.Model):
 	_inherit = 'res.partner'
 
-	date_of_birth = fields.Date(string='Date of Birth')
+	date_of_birth = fields.Date(string='Website Date of Birth')
